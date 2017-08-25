e9be58397bdde12d2a23664d9b7e8fbfd5ee7cea
kreiton <lpfan2003@gmail.com>
2017-08-25 14:21:59 +0300

    My first commit

---
+++ Git-Test-Project/src/ru/my/test/Main.java
@@ -0,0 +1,11 @@
+package ru.my.test;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Git Hub!");
+	}
+
+}
